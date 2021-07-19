537a747d762a5908e734fed4c97c7197b509128f
Levi <levi.jacoby1991@gmail.com>
2021-07-19 12:36:47 -0500

    Set up Pong.ino for development

--- Pong/Pong.ino
+++ Pong/Pong.ino
@@ -1,6 +1,18 @@
 #include <Arduboy2.h>
+//Global Variables
+Arduboy2 arduboy;
+
 void setup() {
+  arduboy.begin();
+  arduboy.clear();
+  arduboy.setFrameRate(60);
 }
 
 void loop() {
+  if(!arduboy.nextFrame()){
+    return;
+  }
+  arduboy.clear();
+  //Game code here
+  arduboy.display();
 }
